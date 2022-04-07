88890369f87267042ba4d4514ddd58b0269d775a
Leo <leo.mandara@tuni.fi>
2022-04-07 15:56:19 +0300

    add basic StudyModule tests

---
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/StudyModuleTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author leoma
+ */
+public class StudyModuleTest {
+    
+    public StudyModuleTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetters() {
+        int maxCredits = 4;
+        String name = "test course name";
+        String id = "test course id";
+        String groupId = "test course groupId";
+        ArrayList<String> organizers = new ArrayList<>();
+        organizers.add("org");
+        organizers.add("ani");
+        organizers.add("zer");
+        
+        StudyModule testModule = new StudyModule(maxCredits, name, id, groupId, organizers);
+        
+        assertTrue(testModule.getMaxCredits() == 4);
+        assertTrue(testModule.getName().equals(name));
+        assertTrue(testModule.getGroupId().equals(groupId));
+        assertTrue(testModule.getOrganizers().equals(organizers));
+    }
+    
+    @Test
+    public void testCompletionStatus() {
+        int maxCredits = 4;
+        String name = "test course name";
+        String id = "test course id";
+        String groupId = "test course groupId";
+        ArrayList<String> organizers = new ArrayList<>();
+        organizers.add("org");
+        organizers.add("ani");
+        organizers.add("zer");
+        
+        StudyModule testModule = new StudyModule(maxCredits, name, id, groupId, organizers);
+        
+        assertTrue(!testModule.isComplete());
+        
+        testModule.completeModule();
+        
+        assertTrue(testModule.isComplete());
+        
+        testModule.cancelModuleCompletion();
+        
+        assertTrue(!testModule.isComplete());
+    }
+}
