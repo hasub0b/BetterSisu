3f56e07b275a37b8ba38c46722cb5d7775768e30
Jyri Hakala <jyri.hakala@tuni.fi>
2022-04-07 16:41:09 +0300

    Implemented GroupingModule

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/GroupingModule.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+/**
+ *
+ * @author Jykae
+ */
+public class GroupingModule {
+
+}
