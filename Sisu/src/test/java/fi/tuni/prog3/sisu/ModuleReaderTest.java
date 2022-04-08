9b02facdd9986ecca7b3a2babf1bfe1677bd9f6b
Leo <leo.mandara@tuni.fi>
2022-04-08 19:00:50 +0300

    add ModuleReader test file

---
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/ModuleReaderTest.java
@@ -0,0 +1,42 @@
+
+package fi.tuni.prog3.sisu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for ModuleReader
+ *
+ * @author Leo
+ */
+public class ModuleReaderTest {
+    
+    public ModuleReaderTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}
