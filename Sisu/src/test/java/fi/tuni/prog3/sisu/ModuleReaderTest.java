91389b5857cbec9e306865947ee2b0eb4cac12fc
Leo <leo.mandara@tuni.fi>
2022-04-08 20:22:12 +0300

    module reader implemented lacking recursivity

--- Sisu/src/test/java/fi/tuni/prog3/sisu/ModuleReaderTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/ModuleReaderTest.java
@@ -46,19 +46,22 @@ public class ModuleReaderTest {
         BufferedReader testBR = null;
         String jsonString = "";
         try {
-            testBR = new BufferedReader(new FileReader("src/test/resources/courseUnitExample.json"));
+            testBR = new BufferedReader(new FileReader("src/test/resources/ModuleExample.json"));
         } catch (FileNotFoundException e) {
             fail("Test file not found (Module)");
         }
         try {
             String line = testBR.readLine();
             while (line != null) {
-                line = testBR.readLine();
                 jsonString += line;
+                line = testBR.readLine();
             }
         } catch (IOException e) {
             fail("File ended unexpectedly");
         }
+        
+        // The files start and finish with a square bracket. Gson didn't like this
+        jsonString = jsonString.substring(1, jsonString.length()-1);
         
         ModuleReader mod = new ModuleReader();
         Module testModule = mod.fromString(jsonString);
