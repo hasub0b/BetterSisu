2f1ae9992b1b887cfc478f280a52d47d3d4ca89c
Leo <leo.mandara@tuni.fi>
2022-04-08 11:30:07 +0300

    Implement course unit from string test

--- Sisu/src/test/java/fi/tuni/prog3/sisu/CourseUnitReaderTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/CourseUnitReaderTest.java
@@ -1,6 +1,11 @@
 
 package fi.tuni.prog3.sisu;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,8 +39,32 @@ public class CourseUnitReaderTest {
     }
 
     @Test
-    public void testSomeMethod() {
+    public void testFromString() {
+        BufferedReader testBR = null;
+        String jsonString = "";
+        try {
+            testBR = new BufferedReader(new FileReader("src/test/resources/courseUnitExample.json"));
+        } catch (FileNotFoundException e) {
+            fail("Test file not found (Course unit)");
+        }
+        try {
+            String line = testBR.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = testBR.readLine();
+                jsonString += line;
+            }
+        } catch (IOException e) {
+            fail("File ended unexpectedly");
+        }
         
+        CourseUnitReader cur = new CourseUnitReader();
+        CourseUnit testCU = cur.fromString(jsonString);
+        
+        assertEquals(testCU.getName(), "Introduction to Analysis");
+        assertEquals(testCU.getCode(), "MATH.MA.110");
+        assertEquals(testCU.getCourseUnitGroupId(), "uta-ykoodi-47926");
+        assertEquals(testCU.getCredits(), 5);
+        assertEquals(testCU.getId(), "otm-94ffcfc5-0db4-4507-b475-63f290639e04");
     }
-    
 }
