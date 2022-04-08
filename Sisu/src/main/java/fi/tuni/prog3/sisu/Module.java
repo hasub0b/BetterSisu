06d10f8026412b689f7a5b69d71ce47385907c5e
Leo <leo.mandara@tuni.fi>
2022-04-08 18:40:41 +0300

    remove unnecessary white space from Module

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
@@ -30,47 +30,35 @@ public abstract class Module {
      * Getter for module name
      * @return Name of module
      */
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;    }
 
     /**
      * Getter for module id
      * @return id of module
      */
-    public String getId() {
-        return id;
-    }
+    public String getId() {return id;}
 
     /**
      * Getter for groupId of module
      * @return groupId of module
      */
-    public String getGroupId() {
-        return groupId;
-    }
+    public String getGroupId() {return groupId;}
 
     /**
      * Getter for sub-modules
      * @return All sub-modules of this module
      */
-    public ArrayList<Module> getSubModules() {
-        return subModules;
-    }
+    public ArrayList<Module> getSubModules() {return subModules;}
     
     /**
      * Add a module to this module's sub-modules
      * @param sub Sub-module to add
      */
-    public void addSubModule(Module sub) {
-        subModules.add(sub);
-    }
+    public void addSubModule(Module sub) {subModules.add(sub);}
     
     /**
      * Add a CourseUnit to this module's sub-units
      * @param sub Sub-unit to add
      */
-    public void addSubUnit(CourseUnit sub) {
-        subUnits.add(sub);
-    }
+    public void addSubUnit(CourseUnit sub) {subUnits.add(sub);}
 }
