62e99bbdcf305d7ac6e1bfb43e7c7a58736bb878
Leo <leo.mandara@tuni.fi>
2022-04-08 18:34:47 +0300

    add javadocs for module class

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
@@ -4,6 +4,7 @@ package fi.tuni.prog3.sisu;
 import java.util.ArrayList;
 
 /**
+ * Represents a Module in SISU which contains several sub-modules and units.
  *
  * @author Leo
  * @author Jyri
@@ -25,26 +26,50 @@ public abstract class Module {
         this.subUnits = new ArrayList<>();
     }
 
+    /**
+     * Getter for module name
+     * @return Name of module
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for module id
+     * @return id of module
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Getter for groupId of module
+     * @return groupId of module
+     */
     public String getGroupId() {
         return groupId;
     }
 
+    /**
+     * Getter for sub-modules
+     * @return All sub-modules of this module
+     */
     public ArrayList<Module> getSubModules() {
         return subModules;
     }
     
+    /**
+     * Add a module to this module's sub-modules
+     * @param sub Sub-module to add
+     */
     public void addSubModule(Module sub) {
         subModules.add(sub);
     }
     
+    /**
+     * Add a CourseUnit to this module's sub-units
+     * @param sub Sub-unit to add
+     */
     public void addSubUnit(CourseUnit sub) {
         subUnits.add(sub);
     }
