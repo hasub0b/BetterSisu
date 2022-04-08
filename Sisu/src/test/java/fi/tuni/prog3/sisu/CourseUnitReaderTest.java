fe1f9f1dd7f5a8c516d8897122a94078cf00e7f8
Leo <leo.mandara@tuni.fi>
2022-04-08 09:41:02 +0300

    add blank test for CourseUnitReader

---
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/CourseUnitReaderTest.java
@@ -0,0 +1,41 @@
+
+package fi.tuni.prog3.sisu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Leo
+ */
+public class CourseUnitReaderTest {
+    
+    public CourseUnitReaderTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}
