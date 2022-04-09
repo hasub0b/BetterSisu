f90a6c9c3d2c05e0287619b500a2cedbf90eab0c
Leo <leo.mandara@tuni.fi>
2022-04-09 14:29:28 +0300

    add submodule/subunit getter tests

--- Sisu/src/test/java/fi/tuni/prog3/sisu/ModuleReaderTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/ModuleReaderTest.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -75,5 +76,33 @@ public class ModuleReaderTest {
             fail("File ended unexpectedly");
         }
         */
+    }
+    
+    @Test
+    public void testGetSubModuleNames() {
+        String groupId1 = "uta-ok-ykoodi-41176";
+        String groupId2 = "uta-tohjelma-1705";
+        
+        ModuleReader mr = new ModuleReader();
+        
+        ArrayList<String> courseUnitData = mr.getSubGroupIds(groupId1).get("unit");
+        ArrayList<String> moduleData = mr.getSubGroupIds(groupId2).get("module");
+        
+        ArrayList<String> expectedUnits = new ArrayList<>();
+        expectedUnits.add("tut-cu-g-45620");
+        expectedUnits.add("tut-cu-g-45510");
+        expectedUnits.add("tut-cu-g-45474");
+        expectedUnits.add("otm-5381c680-bc69-4400-a578-ec0a79141cd0");
+        expectedUnits.add("otm-4da0e13d-222a-4e14-a239-86b76346ba14");
+
+        ArrayList<String> expectedModules = new ArrayList<>();
+        expectedModules.add("otm-3858f1d8-4bf9-4769-b419-3fee1260d7ff");
+        expectedModules.add("uta-ok-ykoodi-41176");
+        expectedModules.add("uta-ok-ykoodi-41177");
+        expectedModules.add("otm-6c36cb36-1507-44ff-baab-a30ac76ca786");
+        expectedModules.add("otm-35d5a7e1-71c1-456a-8783-9cf8c34262f5");
+        
+        assertEquals(expectedUnits, courseUnitData);
+        assertEquals(expectedModules, moduleData);
     }
 }
