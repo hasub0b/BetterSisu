f9b2b37df9e0debd436000ae36fd53ac27fe570d
Leo <leo.mandara@tuni.fi>
2022-04-12 14:44:25 +0300

    add CourseUnit tests for additional info

--- Sisu/src/test/java/fi/tuni/prog3/sisu/CourseUnitTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/CourseUnitTest.java
@@ -38,14 +38,42 @@ class CourseUnitTest {
                 "courseUnitGroupId",
                 "name",
                 "code",
-                5
+                5,
+                6
         );
 
         assertEquals("id", courseUnit.getId());
         assertEquals("courseUnitGroupId", courseUnit.getCourseUnitGroupId());
         assertEquals("name", courseUnit.getName());
         assertEquals("code", courseUnit.getCode());
-        assertEquals(5, courseUnit.getCredits());
+        assertEquals(5, courseUnit.getMinCredits());
+        assertEquals(6, courseUnit.getMaxCredits());
+    }
+    
+    @Test
+    public void testAdditionalInfo() {
+        CourseUnit courseUnit = new CourseUnit(
+                "id",
+                "courseUnitGroupId",
+                "name",
+                "code",
+                5,
+                6
+        );
+        
+        String content = "Contains a heap of homework";
+        String prerequisite = "Assumes student is smart";
+        String outcome = "Student knows airspeed velocity of unladen swallow";
+        String material = "Book.";
 
+        courseUnit.addContent(content);
+        courseUnit.addPrerequisite(prerequisite);
+        courseUnit.addOutcome(outcome);
+        courseUnit.addMaterial(material);
+        
+        assertEquals(courseUnit.getContent(), content);
+        assertEquals(courseUnit.getPrerequisite(), prerequisite);
+        assertEquals(courseUnit.getOutcome(), outcome);
+        assertEquals(courseUnit.getMaterial(), material);
     }
 }
