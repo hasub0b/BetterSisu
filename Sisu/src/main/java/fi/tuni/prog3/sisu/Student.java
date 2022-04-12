a2bb89d843b9899b62659c5c678a7d29a252a956
Jyri Hakala <jyri.hakala@tuni.fi>
2022-04-12 17:07:54 +0300

    implement basic structure for Student

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Student.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Student.java
@@ -1,10 +1,44 @@
 package fi.tuni.prog3.sisu;
 
+import java.util.ArrayList;
+
 /**
  * Describes a student and their chosen DegreeProgramme(s)
  *
  * @author Jyri
  */
 public class Student {
+
+    private final String firstName;
+    private final String lastName;
+    private final String studentId;
+    private ArrayList<DegreeProgramme> programmes;
+
+    public Student(String firstName, String lastName, String studentId, ArrayList<DegreeProgramme> programmes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+        this.programmes = programmes;
+    }
+
+    public Student(String firstName, String lastName, String studentId) {
+        this(firstName, lastName, studentId, new ArrayList<DegreeProgramme>());
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public ArrayList<DegreeProgramme> getProgrammes() {
+        return programmes;
+    }
 
 }
