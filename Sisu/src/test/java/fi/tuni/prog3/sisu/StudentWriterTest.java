7b4952adfed62bb88614b862b80471ed24132319
Jyri Hakala <jyri.hakala@tuni.fi>
2022-04-22 15:25:19 +0300

    implement tests for StudentWriter

--- Sisu/src/test/java/fi/tuni/prog3/sisu/StudentWriterTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisu/StudentWriterTest.java
@@ -1,10 +1,13 @@
 package fi.tuni.prog3.sisu;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 /**
  *
@@ -14,7 +17,11 @@ public class StudentWriterTest {
 
     private static final String PROGRAMME_ID = "uta-tohjelma-1705";
     private static final ModuleReader MR = new ModuleReader();
+    // private static final DegreeProgramme DP = (DegreeProgramme) MR.buildModule(PROGRAMME_ID);
     private static final DegreeProgramme DP = (DegreeProgramme) MR.fromSisu(PROGRAMME_ID);
+
+    @TempDir
+    static Path tempDir;
 
     public StudentWriterTest() {
     }
@@ -25,13 +32,44 @@ public class StudentWriterTest {
     @Test
     public void testWrite() {
         DegreeProgramme dptwo = new DegreeProgramme(2, "dp kaks", "dp kaks id", "dp kaks gid");
-        Student test = new Student("etunimi", "sukunimi", "h123", new ArrayList<>(List.of(DP, dptwo)));
+        String testStudentId = "h123";
+        Student test = new Student("etunimi", "sukunimi", testStudentId, new ArrayList<>(List.of(DP, dptwo)));
         StudentWriter sw = new StudentWriter();
+        String tdString = tempDir.toString();
         try {
-            sw.write(test);
+            System.err.println("Writing to: " + tdString);
+            sw.write(tdString, test);
         } catch (IOException ex) {
             fail("Couldn't write file");
         }
+
+        Path expectedFile = tempDir.resolve(testStudentId + ".json");
+        assertTrue(expectedFile.toFile().exists());
     }
 
+    @Test
+    public void testWriteAll() {
+        String testStudentId = "h123";
+        Student test = new Student("etunimi", "sukunimi", testStudentId, new ArrayList<>(List.of(DP)));
+
+        String anotherStudentId = "K098";
+        Student anotherStudent = new Student("first", "last", anotherStudentId, new ArrayList<>());
+
+        Collection<Student> testStudents = new ArrayList<>(List.of(test, anotherStudent));
+
+        StudentWriter sw = new StudentWriter();
+        String tdString = tempDir.toString();
+        try {
+            System.err.println("Writing to: " + tdString);
+            sw.writeAll(tdString, testStudents);
+        } catch (IOException ex) {
+            fail("Couldn't write file");
+        }
+
+        Path expectedFile = tempDir.resolve(testStudentId + ".json");
+        assertTrue(expectedFile.toFile().exists());
+
+        Path otherExpectedFile = tempDir.resolve(anotherStudentId + ".json");
+        assertTrue(otherExpectedFile.toFile().exists());
+    }
 }
