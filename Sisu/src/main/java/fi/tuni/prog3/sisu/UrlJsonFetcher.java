41a57e48f6dbdfbc8c5ba9d49b82b36461f1117e
Leo <leo.mandara@tuni.fi>
2022-04-27 10:39:22 +0300

    handle errors in URL reading properly

--- Sisu/src/main/java/fi/tuni/prog3/sisu/UrlJsonFetcher.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/UrlJsonFetcher.java
@@ -17,8 +17,13 @@ public class UrlJsonFetcher implements JsonStringFetcher {
      * @return JSON format string of all courses in SISU
      */
     public static String getDegreeList() {
-        String jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/module-search?curriculumPeriodId=uta-lvv-2021&universityId=tuni-university-root-id&moduleType=DegreeProgramme&limit=1000");
-        
+        String jsonString;
+        try {
+            jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/module-search?curriculumPeriodId=uta-lvv-2021&universityId=tuni-university-root-id&moduleType=DegreeProgramme&limit=1000");
+        } catch (Exception e) {
+            jsonString = "{\"searchResults\": []}";
+            System.err.println(e.getMessage());
+        }
         return jsonString;
     }
     
@@ -28,8 +33,15 @@ public class UrlJsonFetcher implements JsonStringFetcher {
      * @return JSON format string of module with groupId from SISU
      */
     public String getModule(String groupId) {
-        String jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/modules/by-group-id?groupId="+groupId+"&universityId=tuni-university-root-id");
-        jsonString = jsonString.substring(1,jsonString.length()-1);
+        String jsonString;
+        try {
+            jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/modules/by-group-id?groupId="+groupId+"&universityId=tuni-university-root-id");
+            jsonString = jsonString.substring(1,jsonString.length()-1);
+        } catch (Exception e) {
+            LocalJsonFetcher ljf = new LocalJsonFetcher();
+            jsonString = ljf.getModule("module-not-found");
+            System.err.println(e.getMessage());
+        }
         return jsonString;
     }
     
@@ -39,8 +51,15 @@ public class UrlJsonFetcher implements JsonStringFetcher {
      * @return JSON format string of course with groupId from SISU
      */
     public String getCourseUnit(String groupId) {
-        String jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/course-units/by-group-id?groupId="+groupId+"&universityId=tuni-university-root-id");
-        jsonString = jsonString.substring(1,jsonString.length()-1);
+        String jsonString;
+        try {
+            jsonString = getStringFromUrl("https://sis-tuni.funidata.fi/kori/api/course-units/by-group-id?groupId="+groupId+"&universityId=tuni-university-root-id");
+            jsonString = jsonString.substring(1,jsonString.length()-1);
+        } catch (Exception e) {
+            LocalJsonFetcher ljf = new LocalJsonFetcher();
+            jsonString = ljf.getCourseUnit("course-unit-not-found");
+            System.err.println(e.getMessage());
+        }
         return jsonString;
     }
     
@@ -49,22 +68,14 @@ public class UrlJsonFetcher implements JsonStringFetcher {
      * @param url URL to fetch string from
      * @return String of contents from URL
      */
-    public static String getStringFromUrl(String url) {
+    private static String getStringFromUrl(String url) throws MalformedURLException, IOException {
         URL urlObj = null;
         InputStream input;
         String contents;
         
-        try {
-            urlObj = new URL(url);
-            input = urlObj.openStream();
-            contents = new String(input.readAllBytes());
-            return contents;
-        } catch (MalformedURLException e) {
-            System.err.println(e);
-            return "Invalid URL.";
-        } catch (IOException e) {
-            System.err.println(e);
-            return "Invalud URL.";
-        }
+        urlObj = new URL(url);
+        input = urlObj.openStream();
+        contents = new String(input.readAllBytes());
+        return contents;
     }
 }
